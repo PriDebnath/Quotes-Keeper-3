b23dc837bd224ef20371730b678c74e7680a3503
Pritam <debnathpritam0802@gmail.com>
2026-01-03 13:23:22 +0530

    edited vite config. Followed link: - https://ui.shadcn.com/docs/installation/vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,11 +6,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    react(
+      {
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }
+    ),
     tailwindcss(),
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
