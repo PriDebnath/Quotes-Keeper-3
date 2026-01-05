be508e8bcc745c42f20b3e5f6edd2cea339fcb88
Pritam <debnathpritam0802@gmail.com>
2026-01-05 16:16:43 +0530

    Add base path configuration for Vite to align with repository structure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Quotes-Keeper-3/", // 👈 EXACT repo name
   plugins: [
     react(
       {
