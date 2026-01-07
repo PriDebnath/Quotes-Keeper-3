590447d381e6edf11f15c2f426e321ef21469def
Pritam <debnathpritam0802@gmail.com>
2026-01-07 15:41:25 +0530

    Linked all schemas to drizzle.

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -2,9 +2,10 @@
 import { Pool } from "pg"
 import { env } from '@/src/utils/env';
 import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from "@/src/database/schema/index"
 
 export const pool = new Pool({
-  connectionString: env.DATABASE_URL
+    connectionString: env.DATABASE_URL
 })
 
-export const db = drizzle(pool)
+export const db = drizzle(pool, { schema: schema })
---
+++ backend/src/database/model/quote/quote.model.ts
@@ -0,0 +1,3 @@
+import type { quotes } from "@/src/database/schema/quote/quote.schema"
+
+export type Quote = typeof quotes.$inferSelect
